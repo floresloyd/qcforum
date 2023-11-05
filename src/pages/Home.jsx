336c09e422ea968702ba3e647022c60a27367634
Loyd <us.flores.loyd@gmail.com>
2023-11-04 21:29:08 -0400

    Navbar Layout, Home Layout, Post Layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import "./Home.css";
 function Home() {
-  return <div>HOME</div>;
+  return (
+    <div className="centered-container">
+      <h1> CUNY Queens College Forum</h1>
+      <h2> </h2>
+    </div>
+  );
 }
 
 export default Home;
